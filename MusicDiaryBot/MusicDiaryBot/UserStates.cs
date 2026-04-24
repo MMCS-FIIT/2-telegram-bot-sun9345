d49513ca877e943a5ceb7259c327c8d751d4438e
sun9345 <skorodiloffandrey@gmail.com>
2026-04-24 20:24:25 +0300

    Add states enum
    states for adding a track into library

---
+++ MusicDiaryBot/MusicDiaryBot/UserStates.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicDiaryBot
+{
+    public enum DialogState
+    {
+        None,
+        AwaitingArtistname,
+        AwaitingSongtitle,
+    }
+
+    public class UserState
+    {
+        public DialogState State {  get; set; } = DialogState.None;
+        public string Artistname { get; set; } = ""; 
+        public string Songtitle { get; set; } = "";
+    }
+}
